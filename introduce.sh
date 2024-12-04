001b03e4809cb72f2ae48261d4db4366c41a6810
Ubuntu <ubuntu@ip-172-31-27-64.ec2.internal>
2024-12-04 22:43:34 +0000

    Provided script with 5 variables and replaced in the text

---
+++ introduce.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+name=Aiganym
+age=22
+fav_language=python
+hobby=cooking
+dream=traveling
+
+echo "Hi,my name is $name, and I'm $age years old. Every morning, $name like to spend time $hobby."
+echo "As a $age-year-old, I believe it's never too late to follow your dreams. My favourite programming language is $fav_language."
+echo "I code in $fav_language almost every day!
+echo "One day, I hope my love for $fav__language will help me achieve my dream of $dream."
+echo "When I am not busy working toward $dream, you will find me $hobby to relax."
+echo "That is a little about me, $name — a $age-year-old who loves $fav_language, enjoys $hobby, and dreams of $dream! "
